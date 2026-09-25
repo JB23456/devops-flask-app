16be52e9d6bb391283180a16172fd89d46040e28
JB23456 <C24733535@mytudublin.ie>
2026-09-25 11:00:02 +0100

    Add contact route and update home page links

--- hello.py
+++ hello.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello, World, I am a Flask app!<p><br><a href="/about">About</a>'
+	return '<p>Hello, World, I am a Flask app!<p><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
 
 @app.route('/about')
 def talk_about():
 	return '<p>This app runs using the Flask web framework<p><br><a href="https://flask.palletsprojects.com/en/stable/">Check out the framework here</a>'
+
+
+@app.route('/contact')
+def contact_info():
+	return '<p>Email: C24733535@mytudublin.ie<p>'
